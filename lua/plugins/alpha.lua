a4934854d4f0e3426cca79d739d187f2d3e7e9f3
Romedius <r@romedius.xyz>
2025-05-20 09:13:31 +0200

    Update alpha.nvim
    Remove recent files and lazy extras

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -29,11 +29,9 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button("f", " " .. " Find file",       "<cmd> lua LazyVim.pick()() <cr>"),
       dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
-      dashboard.button("r", " " .. " Recent files",    [[<cmd> lua LazyVim.pick("oldfiles")() <cr>]]),
       dashboard.button("g", " " .. " Find text",       [[<cmd> lua LazyVim.pick("live_grep")() <cr>]]),
       dashboard.button("c", " " .. " Config",          "<cmd> lua LazyVim.pick.config_files()() <cr>"),
       dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
-      dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
       dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
     }
       if vim.fn.isdirectory("~/.config/hypr") then
