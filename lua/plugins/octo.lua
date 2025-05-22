be6827a32d0294f04b0757c626a5355a3782cb36
Romedius <r@romedius.xyz>
2025-05-22 10:47:46 +0200

    Update octo mappings
    Bugfix reload mapping
    
    Actual pr/issue search and mappings for open in browser and url copy

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -10,12 +10,58 @@ return {
       picker = "telescope",
     },
     keys = {
-      { "<leader>gi", "<cmd>Octo issue list assignee=Rom3dius<CR>", desc = "List Issues (Octo)" },
-      { "<leader>gI", "<cmd>Octo issue search<CR>", desc = "Search Issues (Octo)" },
+      { "<leader>gi", "<cmd>Octo issue list<CR>", desc = "List Issues (Octo)" },
+      { "<leader>gI", "<cmd>Octo issue search involves:@me<CR>", desc = "List my Issues (Octo)" },
       { "<leader>gp", "<cmd>Octo pr list<CR>", desc = "List PRs (Octo)" },
-      { "<leader>gP", "<cmd>Octo pr search<CR>", desc = "Search PRs (Octo)" },
+      { "<leader>gP", "<cmd>Octo pr search involves:@me<CR>", desc = "Search my PRs (Octo)" },
       { "<leader>gr", "<cmd>Octo repo list<CR>", desc = "List Repos (Octo)" },
-      { "<leader>gS", "<cmd>Octo search<CR>", desc = "Search (Octo)" },
+      { "<leader>gnp", "<cmd>Octo pr create<CR>", desc = "Create PR (Octo)" },
+      { "<leader>gni", "<cmd>Octo issue create<CR>", desc = "Create Issue (Octo)" },
+      {
+        "<leader>gu",
+        function()
+          local buftype = vim.api.nvim_buf_get_name(0)
+          if buftype:match("octo://.*/issue/") then
+            vim.cmd("Octo issue reload")
+          elseif buftype:match("octo://.*/pull/") then
+            vim.cmd("Octo pr reload")
+          else
+            vim.notify("Not an Octo issue or PR buffer", vim.log.levels.WARN)
+          end
+        end,
+        desc = "Reload Issue/PR (Octo)",
+        ft = "octo",
+      },
+      {
+        "<leader>gbo",
+        function()
+          local buftype = vim.api.nvim_buf_get_name(0)
+          if buftype:match("octo://.*/issue/") then
+            vim.cmd("Octo issue browser")
+          elseif buftype:match("octo://.*/pull/") then
+            vim.cmd("Octo pr browser")
+          else
+            vim.notify("Not an Octo issue or PR buffer", vim.log.levels.WARN)
+          end
+        end,
+        desc = "Open Issue/PR in browser (Octo)",
+        ft = "octo",
+      },
+      {
+        "<leader>gbc",
+        function()
+          local buftype = vim.api.nvim_buf_get_name(0)
+          if buftype:match("octo://.*/issue/") then
+            vim.cmd("Octo issue url")
+          elseif buftype:match("octo://.*/pull/") then
+            vim.cmd("Octo issue url")
+          else
+            vim.notify("Not an Octo issue or PR buffer", vim.log.levels.WARN)
+          end
+        end,
+        desc = "Copy Issue/PR URL (Octo)",
+        ft = "octo",
+      },
 
       { "<localleader>a", "", desc = "+assignee (Octo)", ft = "octo" },
       { "<localleader>c", "", desc = "+comment/code (Octo)", ft = "octo" },
